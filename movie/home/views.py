921a524fb960faf2ec832a8b32ece9a5f9f43101
Sheriff_Hood <h77max@163.com>
2018-03-28 22:59:11 +0800

    add front html

--- movie/home/views.py
+++ movie/home/views.py
@@ -25,8 +25,20 @@ def register():
 
 @home_blueprint.route('/user/', methods=['GET', 'POST'])
 def user():
-    pass
+    return render_template('home/user.html')
 
-@home_blueprint.route('/serach/<int:page>/')
-def search(page=None):
-    pass
+@home_blueprint.route('/password/')
+def password():
+    return render_template('home/password.html')
+
+@home_blueprint.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+@home_blueprint.route('/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+@home_blueprint.route('/moviecol/')
+def moviecol():
+    return render_template('home/moviecol.html')
