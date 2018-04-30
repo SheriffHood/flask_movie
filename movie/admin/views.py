cb7348bf6a52772106cc419e6a9b60e515525920
Sheriff_Hood <h77max@163.com>
2018-04-30 19:52:21 +0800

    finish tag CURD

--- movie/admin/views.py
+++ movie/admin/views.py
@@ -64,12 +64,42 @@ def tag_add():
 
 @admin_blueprint.route('/tag/list/<int:page>/', methods=['GET'])
 @admin_login_required
-def tag_list(page):
-    if page == None:
+def tag_list(page=None):
+    if page is None:
         page = 1
-    page_data = Tag.query.order_by(Tag.addtime.desc()).paginate(page=page, per_page=1)
+    page_data = Tag.query.order_by(Tag.addtime.desc()).paginate(page=page, per_page=10)
     return render_template('admin/tag_list.html', page_data=page_data)
 
+@admin_blueprint.route('/tag/del/<int:id>/', methods=['GET'])
+@admin_login_required
+def tag_del(id=None):
+    tag = Tag.query.filter_by(id=id).first_or_404()
+    db.session.delete(tag)
+    db.session.commit()
+
+    flash("delete tag successfully", "ok")
+    return redirect( url_for('admin.tag_list', page=1) )
+
+@admin_blueprint.route('/tag/edit/<int:id>/', methods=['GET', 'POST'])
+@admin_login_required
+def tag_edit(id=None):
+    form = TagForm()
+    tag = Tag.query.filter_by(id=id).first_or_404()
+
+    if form.validate_on_submit():
+        data = form.data
+        tag_count = Tag.query.filter_by(name=data['name']).count()
+        if tag.name != data['name'] and tag_count == 1:
+            flash('Tag already exists', 'err')
+            return redirect( url_for('admin.tag_edit', id=id) )
+        tag.name= data['name']
+
+        db.session.add(tag)
+        db.session.commit()
+        flash('Edit Tag successfully', 'ok')
+        return redirect( url_for('admin.tag_edit', id=id) )
+    return render_template('admin/tag_edit.html', form=form, tag=tag)
+    
 @admin_blueprint.route('/movie/add/')
 @admin_login_required
 def movie_add():
